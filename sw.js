da4055accdc3e01aecae626d80f0e5beecc4d91a
TarasBulba32 <140403868+TarasBulba32@users.noreply.github.com>
2023-07-24 18:27:45 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(["aglavnaya.html"]); toolbox.router.get("/img/*", toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
